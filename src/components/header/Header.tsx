e4ac0a9f5ceff3364a9e8228833365d06611d336
Andrey Belkin <and.belkin72@gmail.com>
2022-07-01 11:20:48 +0500

    created NavbarItem.tsx && Navbar.css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from "react";
 import "./Header.css";
+import NavbarItem from "./NavbarItem";
 
 const Header: FC = () => {
   return (
     <header className='header container'>
       <p>Logo</p>
-      <p>Navigation</p>
+      <NavbarItem>Home</NavbarItem>
     </header>
   );
 };
