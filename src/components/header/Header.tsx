c3e716e72faa58ef428249a78263d424678f4418
Andrey Belkin <and.belkin72@gmail.com>
2022-07-01 11:00:04 +0500

    created Header.tsx && Header.css

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import "./Header.css";
+
+const Header: FC = () => {
+  return (
+    <header className='header container'>
+      <p>Logo</p>
+      <p>Navigation</p>
+    </header>
+  );
+};
+
+export default Header;
