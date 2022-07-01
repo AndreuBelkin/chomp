e153e06e0a10e65da28f00799d084bc5e9ffeead
Andrey Belkin <and.belkin72@gmail.com>
2022-07-01 12:41:17 +0500

    created ShoppingCart.tsx && style

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,17 +1,19 @@
-import React, { FC, useEffect } from "react";
+import React, { FC } from "react";
 import NavbarItem from "./NavbarItem";
 import "./Navbar.css";
+import ShoppingCart from "../../app/features/shopping/ShoppingCart";
 
 const Navbar: FC = () => {
   const navbarItems = ["Home", "Order", "Company", "FAQ", "Contact"];
 
-  useEffect(() => {});
   const renderItems = () => {
     return navbarItems.map((item, index) => <NavbarItem key={index}>{item}</NavbarItem>);
   };
+
   return (
     <div className='navigation'>
       <div className='navigation-bar'>{renderItems()}</div>
+      <ShoppingCart />
     </div>
   );
 };
