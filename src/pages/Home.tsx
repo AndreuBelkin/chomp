afb03824891c92ee4f7da354af9fe1e2f6f41e4e
Andrey Belkin <and.belkin72@gmail.com>
2022-07-05 08:23:28 +0500

    created Order section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import About from "../components/sections/About";
 import HowItWork from "../components/sections/HowItWork";
 import Intro from "../components/sections/Intro";
 import Menu from "../components/sections/Menu";
+import Order from "../components/sections/Order";
 import MenuCard from "../store/slices/menu/MenuCard";
 import "./Home.css";
 
@@ -13,9 +14,7 @@ const Home: FC = () => {
       <About />
       <HowItWork />
       <Menu />
-      <section className='section'>
-        <div className='container'>Order section</div>
-      </section>
+      <Order />
       <section className='section section_grey'>
         <div className='container'>Call section</div>
       </section>
